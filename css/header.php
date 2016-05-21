df5f77924c9a50c13804df326ffeeebae62dd8eb
maitabar <11b.smadiyar@gmail.com>
2016-05-21 14:08:16 +0600

    Add files via upload

---
+++ css/header.php
@@ -0,0 +1,3 @@
+<ul class="menu">
+	<li>Mine page</li><li>Friends</li><li>Message</li><li>Audio</li><li>Videos</li>
+</ul>
---
+++ css/menu.php
@@ -0,0 +1,7 @@
+<ul class="menu_ver">
+	<a href="main.php"><li>Mine page</li></a>
+	<a href="friends.php"><li>Friends</li></a>
+	<a href="messages.php"><li>Message</li></a>
+	<a href="audio.php"><li>Audio</li></a>
+	<a href="video.php"><li>Videos</li></a>
+</ul>
